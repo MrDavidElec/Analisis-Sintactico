918793cf12a8cd822898b50bf3befdedae88f9c9
MrDavidElec <davolara6@gmail.com>
2025-10-07 21:43:51 -0600

    Update G6.php

--- G6.php
+++ G6.php
@@ -2,12 +2,16 @@
 if (isset($_POST['expresion'])) {
     $expresion = trim($_POST['expresion']);
 
-    // Expresión regular extendida: acepta id+id, id-id*id/id
+    // /^ inicio de cadena a comparar
+    // id  recibe un id  
+    // (\s*[\+\-\*\/]\s*id) acepta los caracteres seguido de un id 
+    // $ termino de cadena
+    
     if (preg_match('/^id(\s*[\+\-\*\/]\s*id)+$/', $expresion)) {
         echo "<p class='correcto'>✅ Correcto</p>";
     } else {
         echo "<p class='incorrecto'>❌ Incorrecto</p>";
     }
 } else {
-    echo "<p class='incorrecto'>❌ No se recibió ninguna expresión</p>";
+    echo "<p class='incorrecto'>❌ No se recibio ninguna expresion</p>";
 }
